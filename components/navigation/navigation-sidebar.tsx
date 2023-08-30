1f679162a3d887c295ce4e1f960571bd7e08d546
Maestro1982 <kris.tote@telenet.be>
2023-08-30 13:25:58 +0200

    modify layout.tsx in app main folder, create action-tooltip.tsx in components folder, navigation-action.tsx and navigation-sidebar.tsx in components navigation folder

---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,34 @@
+import { redirect } from 'next/navigation';
+
+import { Separator } from '@/components/ui/separator';
+
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+
+import NavigationAction from './navigation-action';
+
+const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect('/');
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  return (
+    <div className='space-y-4 flex flex-col items-center h-full w-full text-primary dark:bg-[#1E1F22] py-3'>
+      <NavigationAction />
+      <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto' />
+    </div>
+  );
+};
+export default NavigationSidebar;
