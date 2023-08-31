ecdf2b5029663bc40b34a37566ffb43bdb2dc6a9
Maestro1982 <kris.tote@telenet.be>
2023-08-31 13:38:57 +0200

    modify invite-modal.tsx in components modals folder, schema.prisma in prisma folder, create page.tsx in app invite routes invite inviteCode folder, route.ts in app api servers serverId invite-code folder

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useState } from 'react';
-import { Copy, RefreshCw } from 'lucide-react';
+import { Check, Copy, RefreshCw } from 'lucide-react';
+import axios from 'axios';
 
 import {
   Dialog,
@@ -17,7 +18,7 @@ import { useModal } from '@/hooks/use-modal-store';
 import { useOrigin } from '@/hooks/use-origin';
 
 export const InviteModal = () => {
-  const { isOpen, onClose, type, data } = useModal();
+  const { isOpen, onOpen, onClose, type, data } = useModal();
   const origin = useOrigin();
 
   const isModalOpen = isOpen && type === 'invite';
@@ -27,6 +28,30 @@ export const InviteModal = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  const onNew = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.patch(
+        `/api/servers/${server?.id}/invite-code`
+      );
+
+      onOpen('invite', { server: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -42,14 +67,21 @@ export const InviteModal = () => {
           </Label>
           <div className='flex items-center mt-2 gap-x-2'>
             <Input
+              disabled={isLoading}
               className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
               value={inviteUrl}
             />
-            <Button size='icon'>
-              <Copy className='w-4 h-4' />
+            <Button disabled={isLoading} onClick={onCopy} size='icon'>
+              {isCopied ? (
+                <Check className='w-4 h-4' />
+              ) : (
+                <Copy className='w-4 h-4' />
+              )}
             </Button>
           </div>
           <Button
+            onClick={onNew}
+            disabled={isLoading}
             variant='link'
             size='sm'
             className='text-xs text-zinc-500 mt-4'
