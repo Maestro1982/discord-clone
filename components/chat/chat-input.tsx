54a5c198e02b194567b2ea173c6ae3dfa3403cb2
Maestro1982 <kris.tote@telenet.be>
2023-09-12 15:26:44 +0200

    modify page.tsx in app main routes servers serverId channels channelId folder, file-upload.tsx, chat-input.tsx in components folder, modal-provider.tsx in components providers folder, use-modal-store.ts in hooks folder, create emoji-picker.tsx in components folder, chat-messages.tsx and chat-welcome.tsx in components chat folder, message-file-modal.tsx in components modal folder, current-profile-pages.ts in lib folder messages.ts in pages api socket folder

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -5,10 +5,14 @@ import * as z from 'zod';
 import axios from 'axios';
 import qs from 'query-string';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Plus, Smile } from 'lucide-react';
+import { Plus } from 'lucide-react';
 
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { EmojiPicker } from '@/components/emoji-picker';
+
+import { useModal } from '@/hooks/use-modal-store';
+import { useRouter } from 'next/navigation';
 
 interface ChatInputProps {
   apiUrl: string;
@@ -22,6 +26,9 @@ const formSchema = z.object({
 });
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const { onOpen } = useModal();
+  const router = useRouter();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -39,6 +46,8 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
       });
 
       await axios.post(url, values);
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
@@ -56,7 +65,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                 <div className='relative p-4 pb-6'>
                   <button
                     type='button'
-                    onClick={() => {}}
+                    onClick={() => onOpen('messageFile', { apiUrl, query })}
                     className='absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center'
                   >
                     <Plus className='text-slate-50 dark:text-[#313338]' />
@@ -70,7 +79,11 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     {...field}
                   />
                   <div className='absolute top-7 right-8'>
-                    <Smile />
+                    <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value} ${emoji}`)
+                      }
+                    />
                   </div>
                 </div>
               </FormControl>
